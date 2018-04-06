c136e47950e3fb04b8bafc79abc54139be020934
longhowlam <longhowlam@gmail.com>
2018-04-06 14:57:24 +0200

    install neo4j

--- localneo.sh
+++ localneo.sh
@@ -1,3 +1,11 @@
+######## installing neo4j
+
+wget -O - https://debian.neo4j.org/neotechnology.gpg.key | sudo apt-key add -
+echo 'deb http://debian.neo4j.org/repo stable/' | sudo tee -a /etc/apt/sources.list.d/neo4j.list
+sudo apt-get update
+sudo apt-get install neo4j
+
+
 ######### A sample session with neo4j
 
 ######### start neo4j
